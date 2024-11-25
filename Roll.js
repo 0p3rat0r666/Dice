0c522380e38e28ff85a2805ec09635b4d7f9f4e6
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-11-25 12:49:33 -0600

    Ummm? He doesn’t know what’s happening

--- Roll.js
+++ Roll.js
@@ -2,8 +2,17 @@
 // all example use a call back function. try to do that
 
 function rollDice(max){
+  rl.close();
   let roll = Math.floor(Math.random()*(max-1+1))+1;
   console.log(roll);
 };
 
 // i want the input to be the max for rollDice();
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question(`What's your name?`, rollDice);
