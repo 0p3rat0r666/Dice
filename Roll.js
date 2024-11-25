a7b55dde4bfbdf3843a3356e4686be50c693d52f
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-11-25 11:27:42 -0600

    Update Roll.js

--- Roll.js
+++ Roll.js
@@ -1,8 +1,9 @@
+// takes user input and uses it in a function
+// all example use a call back function. try to do that
+
 function rollDice(max){
   let roll = Math.floor(Math.random()*(max-1+1))+1;
-  return roll;
+  console.log(roll);
 };
 
-//idk what im doing ive not done this
-
-rollDice(20);
+// i want the input to be the max for rollDice();
