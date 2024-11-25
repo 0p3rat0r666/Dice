0012f78fe8fea38811c2be4fb71a952f4768239c
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-11-25 14:56:42 -0600

    Update Roll.js

--- Roll.js
+++ Roll.js
@@ -2,10 +2,13 @@
 function rollDice(){
   let max = prompt('What is the max face?');
   let roll = Math.floor(Math.random()*(max-1+1))+1;
-  console.log("||||||||||||||||");
-  console.log("||            ||");
-  console.log("||            ||");
-  console.log(`||    ${roll}     ||`);
+  console.log('|||||||||||||||');
+  console.log('||           ||');
+  console.log('||           ||');
+  console.log(`||     ${roll}     ||`);
+  console.log('||           ||');
+  console.log('||           ||');
+  console.log('|||||||||||||||');
   // dice ill finish later i need to get this tested and see if the input works cause i cant use the right terminal here
 };
 
