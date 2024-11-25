5e17ec5c10d03ac85990fc46f67aa32c177d9fd9
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-11-25 19:05:59 +0000

    Changes to be committed: 	modified:   Roll.js

--- Roll.js
+++ Roll.js
@@ -1,10 +1,14 @@
+console.clear();
 // takes user input and uses it in a function
 // all example use a call back function. try to do that
-
 function rollDice(max){
   rl.close();
   let roll = Math.floor(Math.random()*(max-1+1))+1;
-  console.log(roll);
+  console.log("||||||||||||||||")
+  console.log("||            ||");
+  console.log("||            ||");
+  console.log("||    ${roll}     ||");
+  // dice ill finish later i need to get this tested and see if the input works cause i cant use the right terminal here
 };
 
 // i want the input to be the max for rollDice();
@@ -15,4 +19,13 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-rl.question(`What's your name?`, rollDice);
+rl.question('What is the max?', rollDice);
+
+// dice art
+// ||||||||||||||||
+// ||            ||
+// ||            ||
+// ||      ${roll}     ||
+// ||            ||
+// ||            ||
+// ||||||||||||||||
