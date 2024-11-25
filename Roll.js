3bc7983d51747d902971950049dc28c4ef96a038
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-11-25 14:51:32 -0600

    Update Roll.js
    Finally made it work (had to run it in p5.js to test properly)

--- Roll.js
+++ Roll.js
@@ -1,18 +1,13 @@
-console.clear();
-// takes user input and uses it in a function
-// all example use a call back function. try to do that
-function rollDice(max){
+// takes user input and uses it to pick a random number
+function rollDice(){
   let max = prompt('What is the max face?');
   let roll = Math.floor(Math.random()*(max-1+1))+1;
   console.log("||||||||||||||||");
   console.log("||            ||");
   console.log("||            ||");
-  console.log("||    ${roll}     ||");
+  console.log(`||    ${roll}     ||`);
   // dice ill finish later i need to get this tested and see if the input works cause i cant use the right terminal here
 };
-
-// i want the input to be the max for rollDice();
-
 
 // dice art
 // ||||||||||||||||
