3d1015573de5e8175edb2abe7f681f9d446718e3
X401U1 <106628238+0p3rat0r666@users.noreply.github.com>
2024-12-16 09:47:52 -0600

    Update Htmlver.js
    Changed to where it’ll take the value that is in a text input instead of using a prompt box.

--- Htmlver.js
+++ Htmlver.js
@@ -1,9 +1,11 @@
 // html version
 
 function rollDice() {
-  let max = prompt("What is the max face?");
+  let max = document.getElementById("inp").value;
+  console.log(max);
   let roll = Math.floor(Math.random()*(max))+1;
   document.getElementById("result").innerHTML = roll;
 }
 
 // i wanted to use a text input but i couldnt get it to access the number properly. i will probably switch to text input at a later date
+// i figured out how to do text input
